c244ac0100b70382936fe7a7538d4c6479cfe959
Ríchard Lucas Cordeiro Moreira <richardlcmoreira@gmail.com>
2022-02-25 01:24:44 -0300

    reworked the section-4 // adjusted small bugs

--- css/components/section 4/script.js
+++ css/components/section 4/script.js
@@ -1,1 +1,8 @@
-console.log("Funcionou!")
+document.querySelector("#items")
+.addEventListener("wheel", event => {
+    if(event.deltaY > 0) {
+        event.target.scrollBy(300, 0)
+    } else {
+        event.target.scrollBy(-300, 0)
+    }
+})
